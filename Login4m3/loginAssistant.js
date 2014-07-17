c2a934f4c6b761940a7a0c99380243a1a150d829
ShiehShieh <xie510894496@gmail.com>
2014-07-17 23:03:43 +0800

    A utility to simplify the login active on 4m3.tongji.

---
+++ Login4m3/loginAssistant.js
@@ -0,0 +1,11 @@
+var USERNAME = 1352975;
+var PASSWORD = 510894;
+
+
+function login4m3 (argument) {
+	var userName = document.getElementById('username');
+	var password = document.getElementById('password');
+
+	userName.value = USERNAME;
+	password.value = PASSWORD;
+}
